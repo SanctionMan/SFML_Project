e4aaa01495dbb7d5292920e78f6629422c7c9903
SanctionMan <Masterman9>
2018-01-08 10:27:09 -0500

    SFML Installed and configured fully for debug and release staticly.

--- SFML_Project/Main.cpp
+++ SFML_Project/Main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
@@ -6,6 +7,24 @@ int main()
 {
 	
 	cout << "SFML_Project" << endl;
+
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 
 	return 0;
 }
